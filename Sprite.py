3586af9a1844412899e021774792d7e98ce6b626
kiraoyd <96449692+kiraoyd@users.noreply.github.com>
2022-12-06 14:16:55 -0800

    Removed the old move functions, created a simperl version. Creates a method to return the current location. Removed the unneeded data members. Changes the names of the spriteOn/Off functions to just on/off

--- Sprite.py
+++ Sprite.py
@@ -5,43 +5,22 @@ class Sprite:
         self.__xCoord = startX
         self.__yCoord = startY
         self.__brightness = brightness
-        self.__xMax = 4
-        self.__yMax = 4
-        self.__xMin = 0
-        self.__yMin = 0
 
-    def spriteOn(self):
+
+    def on(self):
         display.set_pixel(self.__xCoord, self.__yCoord, self.__brightness) #need to connect to microbit code
 
-    def spriteOff(self):
+    def off(self):
         display.set_pixel(self.__xCoord, self.__yCoord, 0)
         
     def setBrightness(self, level):
         self.__brightness = level
+
+    def move(self, x,y):
+        self.__xCoord = x
+        self.__yCoord = y
+
+    def getCurrentPosition(self):
+        return {"x":self.__xCoord, "y":self.__yCoord}
     
-    #NEXT: how to deal with steps that take us out of bounds cleanly?
-    def moveLeft(self, steps):
-        self.spriteOff()
-        if(self.__xCoord - steps < self.__xMin):
-            self.__xCoord = self.__xMax + self.__xCoord - steps+1
-            print(self.__xCoord)
-        else:
-            self.__xCoord = self.__xCoord - steps
-        self.spriteOn()
-
-    def moveRight(self, steps):
-        self.spriteOff()
-        self.__xCoord = self.__xCoord + steps
-        self.spriteOn()
-
-    def moveUp(self, steps):
-        self.spriteOff()
-        self.__xCoord = self.__yCoord + steps
-        self.spriteOn()
-
-    def moveDown(self, steps):
-        self.spriteOff()
-        self.__xCoord = self.__yCoord - steps
-        self.spriteOn()
-
-# I should have made a PR!
+   
