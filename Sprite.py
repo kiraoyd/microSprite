4e2c4b41e9676aa63f953f42822d5578212c74a5
Michael Wilson <56896666+michaeltothewilson@users.noreply.github.com>
2022-12-06 22:33:13 +0000

    removed old move methods and stale comments

--- Sprite.py
+++ Sprite.py
@@ -1,6 +1,5 @@
-
-
-#How do we get the microbit code imported?
+# micros:sprite
+# by Kira K & Mike W
 
 class Sprite:
     def __init__(self,startX, startY, brightness):
@@ -8,9 +7,8 @@ class Sprite:
         self.__yCoord = startY
         self.__brightness = brightness
 
-
     def on(self):
-        display.set_pixel(self.__xCoord, self.__yCoord, self.__brightness) #need to connect to microbit code
+        display.set_pixel(self.__xCoord, self.__yCoord, self.__brightness) 
 
     def off(self):
         display.set_pixel(self.__xCoord, self.__yCoord, 0)
@@ -24,30 +22,3 @@ class Sprite:
 
     def getCurrentPosition(self):
         return {"x":self.__xCoord, "y":self.__yCoord}
-    
-    #NEXT: how to deal with steps that take us out of bounds cleanly?
-    def moveLeft(self, steps):
-        self.spriteOff()
-        if(self.xCoord - steps < self.xMin):
-            self.xCoord = self.xMax + self.xCoord - steps+1
-            print(self.xCoord)
-        else:
-            self.xCoord = self.xCoord - steps
-        self.spriteOn()
-
-    def moveRight(self, steps):
-        self.spriteOff()
-        self.xCoord = self.xCoord + steps
-        self.spriteOn()
-
-    def moveUp(self, steps):
-        self.spriteOff()
-        self.xCoord = self.yCoord + steps
-        self.spriteOn()
-
-    def moveDown(self, steps):
-        self.spriteOff()
-        self.xCoord = self.yCoord - steps
-        self.spriteOn()
-
-# I should have made a PR!
