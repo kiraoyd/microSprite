bd79467eb0d201163e108f42cdf7c323189befd9
Michael Wilson <56896666+michaeltothewilson@users.noreply.github.com>
2022-12-06 22:03:52 +0000

    changed data members to private
    also, do we need xMax, yMax, xMin, yMin?

--- Sprite.py
+++ Sprite.py
@@ -2,46 +2,46 @@
 
 class Sprite:
     def __init__(self,startX, startY, brightness):
-        self.xCoord = startX
-        self.yCoord = startY
-        self.brightness = brightness
-        self.xMax = 4
-        self.yMax = 4
-        self.xMin = 0
-        self.yMin = 0
+        self.__xCoord = startX
+        self.__yCoord = startY
+        self.__brightness = brightness
+        self.__xMax = 4
+        self.__yMax = 4
+        self.__xMin = 0
+        self.__yMin = 0
 
     def spriteOn(self):
-        display.set_pixel(self.xCoord, self.yCoord, self.brightness) #need to connect to microbit code
+        display.set_pixel(self.__xCoord, self.__yCoord, self.__brightness) #need to connect to microbit code
 
     def spriteOff(self):
-        display.set_pixel(self.xCoord, self.yCoord, 0)
+        display.set_pixel(self.__xCoord, self.__yCoord, 0)
         
     def setBrightness(self, level):
-        self.brightness = level
+        self.__brightness = level
     
     #NEXT: how to deal with steps that take us out of bounds cleanly?
     def moveLeft(self, steps):
         self.spriteOff()
-        if(self.xCoord - steps < self.xMin):
-            self.xCoord = self.xMax + self.xCoord - steps+1
-            print(self.xCoord)
+        if(self.__xCoord - steps < self.__xMin):
+            self.__xCoord = self.__xMax + self.__xCoord - steps+1
+            print(self.__xCoord)
         else:
-            self.xCoord = self.xCoord - steps
+            self.__xCoord = self.__xCoord - steps
         self.spriteOn()
 
     def moveRight(self, steps):
         self.spriteOff()
-        self.xCoord = self.xCoord + steps
+        self.__xCoord = self.__xCoord + steps
         self.spriteOn()
 
     def moveUp(self, steps):
         self.spriteOff()
-        self.xCoord = self.yCoord + steps
+        self.__xCoord = self.__yCoord + steps
         self.spriteOn()
 
     def moveDown(self, steps):
         self.spriteOff()
-        self.xCoord = self.yCoord - steps
+        self.__xCoord = self.__yCoord - steps
         self.spriteOn()
 
 # I should have made a PR!
