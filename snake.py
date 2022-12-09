c6ed5b2129b924897e1416b2d05de58e51aa55ec
Kira <kirak@pdx.edu>
2022-12-08 22:42:25 -0800

    added functionalit to GroupOfSprites to add a new Sprite to the Group

---
+++ snake.py
@@ -0,0 +1,12 @@
+from Sprite import *
+from GroupOfSprites import *
+
+
+startX = 0
+startY = 4
+snakeBrightness = 9
+
+snakeHead = Sprite(startX, startY, snakeBrightness)
+snakeSprites.append(snakeHead)
+snakeHead.
+
