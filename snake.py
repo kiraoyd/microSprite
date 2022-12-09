26fd8ca7a9962dfb3b4c75db32553a71c0db76ec
Kira <kirak@pdx.edu>
2022-12-08 22:56:09 -0800

    Realised I want the move functions to return the moved too location.

--- snake.py
+++ snake.py
@@ -1,12 +1,33 @@
 from Sprite import *
 from GroupOfSprites import *
+from random import *
+import math
 
 
 startX = 0
 startY = 4
-snakeBrightness = 9
+snakeBrightness = 5
 
+empty = []
+snake = GroupOfSprites(empty, snakeBrightness)
 snakeHead = Sprite(startX, startY, snakeBrightness)
-snakeSprites.append(snakeHead)
-snakeHead.
+snake.addSprite(snakeHead)
+
+alive = true
+
+while (alive):
+
+    #generate an apple
+    appleBrightness = 5
+    appleX = math.floor(4*random())
+    appleY = math.floor(4*random())
+    apple = Sprite(appleX, appleY, appleBrightness)
+    eaten = False
+
+    while(not eaten):
+        if accelerometer.was_gesture('up'):
+            snake.moveUpBy(1)
+            
+
+    
 
